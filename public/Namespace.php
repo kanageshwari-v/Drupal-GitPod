9ef8dbf99f2beb245154fbd16e6b71185fcde0dd
kanageshwari-v <kanageshwari10@gmail.com>
2022-11-13 16:50:08 +0000

    namespace example

---
+++ public/Namespace.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyProject {
+
+function connect() { echo "ONE";  }
+    Sub\Level\connect();
+}
+
+namespace MyProject\Sub {
+   
+function connect() { echo "TWO";  }
+    Level\connect();
+}
+
+namespace MyProject\Sub\Level {
+   
+    function connect() { echo "THREE";  }   
+    \MyProject\Sub\Level\connect(); 
+    connect();
+}
+?>
