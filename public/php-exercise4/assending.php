beb02f1aff7af8bab23b0e57033cba6f64307740
kanageshwari6795 <kanageshwarikk@gmail.com>
2023-01-13 10:32:07 +0000

    php-exercise4

--- public/php-exercise4/assending.php
+++ public/php-exercise4/assending.php
@@ -1,20 +1,10 @@
 <?php
-$arr = [10, 2, 3, 1, 1, 4, 1, 2, 32, 41, 22, 10, 2, 4];
-function group_same_numbers($arr) {
-    $result = [];
-    $count = count($arr);
-    for ($i = 0; $i < $count; $i++) {
-        $result[$arr[$i]] = $result[$arr[$i]] + 1;
-    }
-    ksort($result);
-    $result_arr = [];
-    foreach ($result as $key => $value) {
-        for ($i = 0; $i < $value; $i++) {
-            $result_arr[] = $key;
-        }
-    }
-    return $result_arr;
-}
+$numbers = array(10 ,2 ,3 ,1 ,1 ,4 ,1 ,2 ,32 ,41 ,22 ,10 ,2 ,4);
+sort($numbers);
 
-echo(group_same_numbers($arr));
+$arrlength = count($numbers);
+for($x = 0; $x < $arrlength; $x++) {
+  echo $numbers[$x];
+  echo "<br>";
+}
 ?>
---
+++ public/php-exercise4/min-change-str.php
@@ -0,0 +1,11 @@
+<?php
+$str = "ABCDA";
+$target = "AAAAA";
+$count = 0;
+foreach ($str as $len($target));{
+    if($str[$i] != $target[$i]){
+        $count++;
+    }
+}
+echo $count;
+?>
