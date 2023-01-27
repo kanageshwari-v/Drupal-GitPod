0df98be149b3c0d1096f5f6244323971d2914fbb
kanageshwari6795 <kanageshwarikk@gmail.com>
2023-01-27 17:19:30 +0000

    w3schools

--- public/php-w3school-exercise/non-repeated-chr.php
+++ public/php-w3school-exercise/non-repeated-chr.php
@@ -1,2 +1,12 @@
 <?php
-fi
+function fin_no_repeat($word){
+    $char = null;
+    for($i = 0; $i<= strlen($word); $i++){
+        if(substr_count($word, substr($word, $i,1)) ==1){
+            return substr($word,$i,1);
+        }
+    }
+}
+echo find_no_repeat(Green) ."\n";
+echo find_no_repeat(abcdea) . "\n";
+?>
